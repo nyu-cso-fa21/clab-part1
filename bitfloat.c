22a75e89d4c9c34f7dd31b75058f122189ea60ae
Jinyang Li <jinyang@cs.nyu.edu>
2021-08-27 13:44:36 -0400

    add lab1

---
+++ bitfloat.c
@@ -0,0 +1,40 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <assert.h>
+
+// Return the bit value (0 or 1) at position pos of unsigned int n
+// Note that the least significant bit position is 0. 
+int get_bit_at_pos(unsigned int n, int pos)
+{
+  // TODO: Your code here.
+}
+
+// Set the bit value to 1 at position pos of unsigned int n,
+// while leaving other bits unchanged. 
+// Return the changed integer.
+// Note that the least significant bit position is 0. 
+int set_bit_at_pos(unsigned int n, int pos)
+{
+  // TODO: Your code here.
+}
+
+// Return the most significant byte of unsigned int n
+char get_most_significant_byte(unsigned int n)
+{
+  // TODO: Your code here.
+
+}
+
+// Return true if n1+n2 causes overflow, return false otherwise
+bool sum_overflowed(int n1, int n2)
+{
+  // TODO: Your code here.
+}
+
+//Extract the 8-bit exponent field of single precision floating point number f 
+//and return it as an unsigned byte
+unsigned char get_exponent_field(float f) 
+{
+	//TODO: Your code here.
+}
+
